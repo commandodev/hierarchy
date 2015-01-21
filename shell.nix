1b2c0555b0284c1bc462c3f5bb99ce6c4b770c11
Ben Ford <ben@perurbis.com>
2015-01-21 20:04:51 +0000

    Add some deps and start playing with pipes/fold

--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
 with (import <nixpkgs> {}).pkgs;
 let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, base, containers, lens, stdenv, text }:
+            ({ mkDerivation, base, containers, foldl, lens, pipes, stdenv, text
+             }:
              mkDerivation {
                pname = "hierarchy";
                version = "0.1.0.0";
                src = ./.;
-               buildDepends = [ base containers lens text ];
+               buildDepends = [ base containers foldl lens pipes text ];
                homepage = "https://github.com/boothead/heirarchy";
                license = stdenv.lib.licenses.unfree;
              }) {};
--- src/Hierarchy.hs
+++ src/Hierarchy.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Hierarchy where
 
+import           Control.Applicative
+import qualified Control.Foldl as CF
 import           Control.Lens
 import qualified Data.Foldable as F
 import qualified Data.Map.Lazy as M
+import           Data.Monoid (Monoid(..), Sum(Sum))
 import           Data.Text     (Text)
 
 
@@ -36,8 +40,26 @@ foldTrades trades m = foldr fldMap m trades
 
 test :: M.Map Measure Double
 test = foldTrades trades M.empty
-  where
-  trades = [ Trade "1CAD" M.empty $ M.fromList [("ONE", 1), ("TWO", 10), ("THREE", 1)]
-           , Trade "1CAD" M.empty $ M.fromList [("ONE", 1), ("TWO", 11), ("FOUR", 20)]
-           , Trade "1CAD" M.empty $ M.fromList [("ONE", 1), ("TWO", 10), ("THREE", 0)]
-           ]
+
+ 
+trades :: [Trade]
+trades = [ Trade "1CAD" M.empty $ M.fromList [("ONE", 1), ("TWO", 10), ("THREE", 1)]
+         , Trade "1CAD" M.empty $ M.fromList [("ONE", 1), ("TWO", 11), ("FOUR", 20)]
+         , Trade "1CAD" M.empty $ M.fromList [("ONE", 1), ("TWO", 10), ("THREE", 0)]
+         ]
+
+--trades ^.. traversed.filtered (has $ measures.ix "ONE")
+
+foldMon
+  :: (Monoid s, Rewrapped s s, Traversable f,
+      Unwrapped s ~ Double) =>
+     (Double -> s) -> f Trade -> M.Map Measure Double
+foldMon mon trades =
+  view _Wrapped <$> F.foldr (M.unionWith mappend) M.empty (fmap (view (_Unwrapping mon)) <$> trades ^.. traversed.measures)
+
+range :: (Ord a) => CF.Fold a (Maybe (a, b))
+range = sequenceOf both <$> CF.minimum <*> CF.maximum
+
+
+-- ext :: (Maybe a, Maybe b) -> Maybe (a, b)
+-- ext inp = (,) <$> _
