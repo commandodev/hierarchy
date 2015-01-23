1f6387c17f5d866b95d218cf5927b444924e817a
Ben Ford <ben@perurbis.com>
2015-01-23 19:59:20 +0000

    Add some generic folds

--- src/Hierarchy.hs
+++ src/Hierarchy.hs
@@ -5,14 +5,15 @@
 module Hierarchy where
 
 import           Control.Applicative
-import Control.Foldl (Fold(..))
 import qualified Control.Foldl       as CF
+import           Control.Foldl (Fold(..))
 import           Control.Lens hiding (Fold)
 import qualified Data.Foldable       as F
 import qualified Data.Map.Lazy       as M
 import           Data.Monoid         (Monoid (..), Sum (Sum))
 import           Data.Profunctor
-import Data.Set (Set)
+import           Data.Set (Set)
+import           Data.Maybe
 import           Data.Text           (Text)
 
 
@@ -36,17 +37,6 @@ data Trade = Trade {
 makeLenses ''Trade
 
 
-foldTrades :: [Trade] -> M.Map Measure Double -> M.Map Measure Double
-foldTrades trades m = foldr fldMap m trades
-  where
-  fldMap t m = M.foldrWithKey f m (t ^. measures)
-  f m n totals = M.insertWith (+) m n totals
-
-
-test :: M.Map Measure Double
-test = foldTrades trades M.empty
-
-
 trades :: [Trade]
 trades = [ Trade "1CAD" M.empty $ M.fromList [("ONE", 1), ("TWO", 10), ("THREE", 1)]
          , Trade "1CAD" M.empty $ M.fromList [("ONE", 1), ("TWO", 11), ("FOUR", 20)]
@@ -59,8 +49,8 @@ foldMon
   :: (Monoid s, Rewrapped s s, Traversable f,
       Unwrapped s ~ Double) =>
      (Double -> s) -> f Trade -> M.Map Measure Double
-foldMon mon trades =
-  view _Wrapped <$> F.foldr (M.unionWith mappend) M.empty (fmap (view (_Unwrapping mon)) <$> trades ^.. traversed.measures)
+foldMon mon t =
+  view _Wrapped <$> F.foldr (M.unionWith mappend) M.empty (fmap (view (_Unwrapping mon)) <$> t ^.. traversed.measures)
 
 range :: (Ord a) => CF.Fold a (Maybe (a, a))
 range = fmap (sequenceOf both) $ (,) <$> CF.minimum <*> CF.maximum
@@ -75,3 +65,19 @@ keys
   :: Ord k =>
      Fold (M.Map k a) (Set k)
 keys = Fold M.union M.empty M.keysSet
+
+counts :: Ord k => Fold (M.Map k m) (M.Map k Integer)
+counts = Fold step M.empty id
+  where
+  step acc m = M.foldrWithKey (M.insertWith (+)) (fmap (const 1) m) acc
+
+overMaps :: (Ord k) => Fold a b -> Fold (M.Map k a) (M.Map k b)
+overMaps (Fold step begin done) = Fold step' M.empty (fmap done)   
+  where
+  step' acc m = M.foldrWithKey insert acc m
+  insert k el acc = M.insert k (step (fromMaybe begin $ M.lookup k acc) el) acc
+
+test :: IO ()
+test = print $ CF.fold fld trades
+  where
+  fld = (,) <$> CF.pretraverse measures counts <*> CF.pretraverse measures range 
